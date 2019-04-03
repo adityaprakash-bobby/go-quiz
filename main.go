cfdae820dcff7c8f7a2a3e5a0088a3bff6173c5a
Aditya <adityaprakash.bobby@gmail.com>
2019-04-03 13:57:17 +0530

    First approach

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"encoding/csv"
+	"strings"
+)
+
+type problem struct {
+	ques string
+	ans string
+}
+
+func exit (msg string) {
+	fmt.Printf(msg)
+	os.Exit(1)
+}
+
+func main () {
+	csvFile := flag.String("file", "problems.csv", "A csv file in the format of 'question,answer'")
+	flag.Parse()
+
+	file, err := os.Open(*csvFile)
+
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the csv file : %s\n", *csvFile))
+	}
+
+	reader := csv.NewReader(file)
+
+	lines, err := reader.ReadAll()
+
+	if err != nil {
+		exit("Failed to read the CSV file.\n")
+	}
+
+	problems := parseLines(lines)
+
+	correct := 0 
+
+	for i, problem := range problems {
+		fmt.Printf("Question #%d: %s = \n", i+1, problem.ques)
+
+		var answer string
+
+		fmt.Scanf("%s\n", &answer)
+
+		if answer == problem.ans {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines (lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+
+	for i, line := range lines {
+		ret[i] = problem {
+			ques : line[0],
+			ans : strings.TrimSpace(line[1]),
+		}
+	}
+
+	return ret
+}
